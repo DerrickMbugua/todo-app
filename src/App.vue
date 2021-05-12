3e053c99e77876af087bded29840179f55e581ab
Derrick Mwema <derrickmbugua50@gmail.com>
2021-05-12 07:38:20 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,15 @@
     <input name="newTodo" v-model="newTodo">
     <button>Add New Todo</button>
   </form>
- <h2> {{ newTodo }} </h2>
+  <button @click="markAllDone">Mark All Done</button>
+  <button @click="removeAllTodos">Remove All</button>
+  <ol>
+    <li :class="{ done: todo.done }" v-for="(todo, index) in todos" :key="todo.id">
+   <h3 @click="toggleDone(todo)">{{ todo.content }}</h3>
+   <button @click="removeTodo(index)">Remove Todo</button>
+ </li>
+  </ol>
+ 
 </template>
 
 <script>
@@ -13,26 +21,68 @@ import { ref } from 'vue';
 export default {
   setup(){
     const newTodo = ref('');
-
+    const todos = ref([]);
 
     function addNewTodo(){
-      console.log('form was submitted');
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+
+      });
+      newTodo.value = '';
     }
+
+    function toggleDone(todo){
+      todo.done = !todo.done;
+    }
+
+    function removeTodo(index){
+      todos.value.splice(index, 1);
+    }
+
+    function markAllDone(){
+      todos.value.forEach((todo) => todo.done = true);
+    }
+
+    function removeAllTodos(){
+todos.value = [];
+    }
+
     return{
+      todos,
       newTodo,
       addNewTodo,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      removeAllTodos,
     };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body{
+  font-family: sans-serif;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-size: 2em;
+  width: 80%;
+  margin: 0 auto;
+}
+input, textarea, button, p, div, section, article, select{
+  display: block;
+  width: 100%;
+  font-family: sans-serif;
+  font-size: 1em;
+  margin: 0.5em;
+}
+.done{
+  text-decoration: line-through;
+}
+h3{
+  cursor: pointer;
 }
 </style>
